67fe87be93621287f39e9f9235f04b7ee41dec98
Yuri Stanzani <ystanzani@gmail.com>
2025-03-26 11:01:18 -0300

    test: verify adding two comments

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,29 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    it('Deve inserir dois comentários corretamente', () => {
+        render(<PostComment/>) // Inicializa o componente em um ambiente de teste.
+        
+        //Busca elementos pelo atributo data-testid.
+        const addCommentButton = screen.getByTestId('comment-submit') 
+        const addCommentTextArea = screen.getByTestId('comment-input')
+        const addCommentList = screen.getByTestId('comment-list')
+
+        fireEvent.change(addCommentTextArea, // Simula a digitação no input (value é o texto do comentário).
+            { target: 
+                {value: 'Olá, sou o primeiro comentário'}
+            })
+        fireEvent.click(addCommentButton) // Simula o clique no botão para adicionar o comentário.
+
+        fireEvent.change(addCommentTextArea, 
+            { target: 
+                {value: 'Olá, sou o segundo comentário'}
+            })
+        fireEvent.click(addCommentButton)
+        
+        // Verifica se o texto dos comentários aparece na lista.
+        expect(addCommentList).toHaveTextContent('Olá, sou o primeiro comentário')
+        expect(addCommentList).toHaveTextContent('Olá, sou o segundo comentário')
+        
+    })
 });
